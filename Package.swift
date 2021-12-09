846aac8f465c267b1c33d1958c0da8f0b8cc0991
edward <yanyongkun@linghit.com>
2021-12-09 17:05:29 +0800

    update package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YKSwiftSectionViewModel",
-            dependencies: []),
+            dependencies: [],
+	    resources:[
+ 		.process("ic_sectionvm_nodata_image.png"),],		),
         .testTarget(
             name: "YKSwiftSectionViewModelTests",
             dependencies: ["YKSwiftSectionViewModel"]),
